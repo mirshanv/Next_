97308ee25e6561b87bc77376019a75fb986f923f
Mirshan Muhammed <mirshanmuhammed@Mirshans-MacBook-Pro.local>
2023-12-07 14:32:27 +0530

    login

--- src/app/page.js
+++ src/app/page.js
@@ -4,26 +4,9 @@ import Link from 'next/link';
 import ResponsiveAppBar from '../../Componentes/Header';
 import SignIn from './Sign/SignedIn';
 function CookieExample() {
-  const [myConst, setMyConst] = React.useState('hello')
-  // const click=()=>{
-  //   Cookies.set('username', 'JohnDoe', { expires: 10, path: '/' });
-  // }
-  const rem = () => {
-    // Cookies.remove('username');
-    // Cookies.remove('MIRSHSN');
-
-  }
-  // const username = Cookies.get('username');
-  // console.log('====================================');
-  // console.log(username);
-  // console.log('====================================');
   return (
     <div>
       <SignIn />
-      <h1>Welcome to the Home Page</h1>
-      <Link href="/contact">
-        About
-      </Link>
     </div>
   );
 }
